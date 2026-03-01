63e4f97c2bdbab92863e6037324b14ea73fde6de
Vansh <sainivansh765@gmail.com>
2026-03-02 00:59:56 +0530

    redesigned education component

--- frontend/src/components/edupage/eduinfo.jsx
+++ frontend/src/components/edupage/eduinfo.jsx
@@ -1,4 +1,5 @@
 import "./eduinfo.css";
+import mainImage from "../../assets/vanshImage(1).png";
 import nexticon from "../../assets/next.png";
 function Eduinfo() {
   const Experience = [
@@ -9,84 +10,132 @@ function Eduinfo() {
     },
   ];
 
-
-
   const Education = [
     {
       degree: "Bachelor of Computer Application",
       institute: "Sanatan Dharma College, Ambala Cantt",
       year: "2023 - 2026",
-      info:"score: 8.5 CGPA (till 5th sem)"
+      info: "score: 8.5 CGPA (till 5th sem)",
     },
 
     {
       degree: "Senior Secondary Education ",
       institute: "MPM Public School, Ismailabad(KKR)",
       year: "2022-2023",
-      info:"score: Non-Medical - 67%"
+      info: "score: Non-Medical - 67%",
     },
     {
       degree: "Secondary Education",
       institute: "MPM Public School, Ismailabad(KKR)",
       year: "2020-2021",
-      info:"score: 84%"
+      info: "score: 84%",
     },
   ];
 
+  const Skills = [
+    { name: "ReactJS", progress: 60 },
+    { name: "NodeJS", progress: 50 },
+    { name: "ExpressJS", progress: 60 },
+    { name: "MongoDB", progress: 60 },
+    { name: "HTML", progress: 80 },
+    { name: "CSS", progress: 70 },
+  ];
   return (
     <>
-      <div className="edu-container">
-        <div className="left-section">
-          
-        </div>
-        <div className="right-section">
-          <div className="heading">
-            <p>Vansh Saini</p>
-            <p>Full Stack Developer</p>
-          </div>
-          <div className="main-container">
-            <p>Experience</p>
-
-            <div className="experience-info">
-              {Experience.map((item, index) => {
+      <div className="edu-main-container">
+        <div className="edu-container">
+          <div className="float-container">
+            <div className="float-img">
+              <div className="img-background">
+                <img src={mainImage} alt="" id="float-img" draggable={false} />
+              </div>
+            </div>
+            <div className="float-skills">
+              <div className="float-skills-heading">
+                <p>Skills</p>
+              </div>
+              {Skills.map((item, index) => {
                 return (
-                  <div className="experience-item" key={index}>
-                    <div className="item-left">
-                      <img src={nexticon} alt="" draggable={false} />
+                  <div className="float-skills-data">
+                    <div className="float-skills-left">
+                      {/* <img src={nexticon} alt="" draggable={false} /> */}
+                      <p>{item.name}</p>
                     </div>
-                    <div className="item-right">
-                      <p>{item.Role}</p>
-                      <p>{item.Company}</p>
-                      <p>{item.Time}</p>
+                    <div className="float-skills-right">
+                      <div className="w-full bg-gray-200 rounded-full h-3">
+                        <div
+                          className="bg-blue-500 h-3 rounded-full transition-all duration-500"
+                          style={{ width: `${item.progress}%` }}
+                        ></div>
+                      </div>
                     </div>
                   </div>
                 );
               })}
+
+              {/* <p>JavaScript</p>
+              <p>ReactJS</p>
+              <p>NodeJS</p>
+              <p>ExpressJS</p>
+              <p>MongoDB</p>
+              <p>HTML</p>
+              <p>CSS</p> */}
+            </div>
+            <div className="float-contact">
+              <div className="float-contact-heading"></div>
+              <div className="float-contact-data"></div>
             </div>
           </div>
-          <div className="main-container">
-            <p>Education</p>
-            <div className="edu-info">
-              {Education.map((item, index) => {
-                return (
-                  <div className="edu-item" key={index}>
-                    <div className="edu-item-left">
-                      <img src={nexticon} alt="" draggable={false} />
-                    </div>
-                    <div className="edu-item-right">
-                      <div className="edu-degree-info">
-                        {" "}
-                        <p id="degree-info">{item.degree}</p>
-                        <p>{item.year}</p>
+          <div className="left-section"></div>
+          <div className="right-section">
+            <div className="heading">
+              <p>Vansh Saini</p>
+              <p>Full Stack Developer</p>
+            </div>
+            <div className="main-container">
+              <p>Experience</p>
+
+              <div className="experience-info">
+                {Experience.map((item, index) => {
+                  return (
+                    <div className="experience-item" key={index}>
+                      <div className="item-left">
+                        <img src={nexticon} alt="" draggable={false} />
                       </div>
-                      <div className="edu-institite-info">
-                        <p id="degree-info">{item.institute}</p>
-                        <p>{item.info}</p>
+                      <div className="item-right">
+                        <p>{item.Role}</p>
+                        <p>{item.Company}</p>
+                        <p>{item.Time}</p>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
+            </div>
+            <div className="main-container">
+              <p>Education</p>
+              <div className="edu-info">
+                {Education.map((item, index) => {
+                  return (
+                    <div className="edu-item" key={index}>
+                      <div className="edu-item-left">
+                        <img src={nexticon} alt="" draggable={false} />
+                      </div>
+                      <div className="edu-item-right">
+                        <div className="edu-degree-info">
+                          {" "}
+                          <p id="degree-info">{item.degree}</p>
+                          <p>{item.year}</p>
+                        </div>
+                        <div className="edu-institite-info">
+                          <p id="degree-info">{item.institute}</p>
+                          <p>{item.info}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
